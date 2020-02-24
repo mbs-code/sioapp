ce06361dc5e51a51d8a49d7569a637fa767c2fb1
mabasasi <aoe317293@yahoo.co.jp>
2020-02-25 07:20:36 +0900

    [add] 検索機能を実装

--- src/views/videos/Index.vue
+++ src/views/videos/Index.vue
@@ -1,6 +1,26 @@
 <template lang='pug'>
   div
-  
+    //- search panel
+    v-row.mx-2(no-gutters align='center' justify='center')
+      v-col.mx-2(cols='auto')
+        v-text-field(label='search' v-model='search.text' @change='onSearch')
+
+      v-col.mx-2(cols='auto')
+        v-btn-toggle(v-model='search.type' color='primary' dense multiple @change='onSearch')
+          v-btn(value='video' elevation='2') 動画
+          v-btn(value='upcoming' elevation='2') 予約
+          v-btn(value='live' elevation='2') 配信中
+          v-btn(value='archive' elevation='2') アーカイブ
+      
+      v-col.mx-2(cols='auto')
+        v-btn-toggle(v-model='search.status' color='primary' dense multiple @change='onSearch')
+          v-btn(value='public' elevation='2') 公開
+          v-btn(value='unlisted' elevation='2') 限定公開
+          v-btn(value='private' elevation='2') 非公開
+
+      v-col.mx-2(cols='auto')
+        v-btn(@click='onSearch') q
+
     //- header
     v-row.mx-2(no-gutters align='center' justify='center')
       v-col(cols='auto')
@@ -13,17 +33,21 @@
 
       v-col(cols='auto')
         //- 色合いが微妙なので css で弄る (v-item--active)
-        v-btn-toggle(v-model='listMode' dense color='primary')
+        v-btn-toggle.extend(v-model='listMode' dense color='primary')
           v-btn(color='white' elevation='2')
             v-icon(small) mdi-view-grid
           v-btn(color='white' elevation='2')
             v-icon(small) mdi-view-list
 
+    //- main
     VideoList(:videos='videos' :showGrid='listMode === 0' :imageWidth='320')
 
-    v-col(cols='auto')
-      v-pagination(v-model='page' :length='totalPages' total-visible='7' @input='onSearch')
+    //- footer
+    v-row.mx-2(no-gutters align='center' justify='center')
+      v-col(cols='auto')
+        v-pagination(v-model='page' :length='totalPages' total-visible='7' @input='onSearch')
 
+    Loading(:show='showLoading')
 </template>
 
 <script>
@@ -39,6 +63,12 @@ export default {
 
   data: function() {
     return {
+      search: {
+        text: '',
+        type: [],
+        status: []
+      },
+      showLoading: true, // loading flag
       listMode: 1, // 0: grid, 1:list
       videos: [],
       page: 1, // 表示しているページ番号
@@ -58,14 +88,17 @@ export default {
 
     async getDataFromApi() {
       const ts = new Date()
+      this.showLoading = true
 
       try {
         const { data } = await this.$http.get('videos', {
           params: {
+            text: this.search.text,
+            type: this.search.type,
+            status: this.search.status,
             page: this.page,
             sort: 'startTime',
-            order: 'desc',
-            type: 'archive'
+            order: 'desc'
           }
         })
 
@@ -76,6 +109,8 @@ export default {
         this.requestTime = new Date() - ts
       } catch (err) {
         this.$toast.error(err)
+      } finally {
+        this.showLoading = false
       }
     }
   }
@@ -85,10 +120,10 @@ export default {
 <style scope lang='scss'>
 // btn group の色を btn と同等にする修正
 // color で濃いめを指定すること
-.v-btn-toggle .v-btn.v-item--active:before {
+.v-btn-toggle.extend .v-btn.v-item--active:before {
   opacity: 1 !important;
 }
-.v-btn-toggle .v-btn.v-item--active i {
+.v-btn-toggle.extend .v-btn.v-item--active i {
   color: white !important;
 }
 </style>
