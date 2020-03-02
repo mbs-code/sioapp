b87e29685f31ff931c3cd0a348e8b3b1f0cd5ef7
mabasasi <aoe317293@yahoo.co.jp>
2020-03-03 02:28:10 +0900

    [mod] 開発モードで api を叩く時、自身の鯖を proxy にして使う
    ・こうしないとスマホバッグができない…

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
  
-axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT
+// axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT
+axios.defaults.baseURL = process.env.NODE_ENV
+  ? 'api/'
+  : process.env.VUE_APP_API_ENDPOINT
 
 Vue.use(VueAxios, axios)
--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,24 @@ const path = require('path')
 
 module.exports = {
   publicPath: './',
-  "transpileDependencies": [
-    "vuetify"
+  devServer: {
+    proxy: {
+      'api': {
+        target: process.env.VUE_APP_API_ENDPOINT,
+        ws: false,
+        changeOrigin: true
+      }
+    }
+  },
+  transpileDependencies: [
+    'vuetify'
   ],
   configureWebpack: {
     resolve: {
       alias: {
         'vue$': 'vue/dist/vue.esm.js',
         '@': path.resolve(__dirname, 'src/')
-      },
-    },
-  },
+      }
+    }
+  }
 }
