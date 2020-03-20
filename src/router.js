a88259c8928d6edc34731008b707a8ae1354b0bb
mabasasi <aoe317293@yahoo.co.jp>
2020-03-21 01:42:39 +0900

    [fix] route に -index が付く問題の修正

--- src/router.js
+++ src/router.js
@@ -9,13 +9,23 @@ const RouterLayout = createRouterLayout(layout => {
   return import('@/layouts/' + layout + '.vue')
 })
 
+// dir root route に -index が付く問題の簡易 fix
+const str = '-index'
+const fixRoutes = routes.map(e => {
+  const name = '' + e.name
+  if (name.indexOf(str)) {
+    e.name = name.replace(str, '')
+  }
+  return e
+})
+
 export default new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
       component: RouterLayout,
-      children: routes
+      children: fixRoutes
     }
   ]
 })
