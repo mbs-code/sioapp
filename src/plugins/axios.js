b4c13df2c3fa226d348712d1c2e23a05243dc124
mabasasi <aoe317293@yahoo.co.jp>
2020-04-19 15:22:03 +0900

    [fix] axios で token が付与されないバグを修正
    ・getter から state に切り替えたときに修正忘れてた可能性
    ・localhost で通常稼働してたのは何故なのか…
    
    ・baseURL を .env から直接参照する形にした

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,13 +4,10 @@ import VueAxios from 'vue-axios'
 
 import store from '@/store'
  
-// axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT
-axios.defaults.baseURL = process.env.NODE_ENV
-  ? '/api/'
-  : process.env.VUE_APP_API_ENDPOINT
+axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT || '/api/'
 
 axios.interceptors.request.use(request => {
-  const isLogin = store.state.isLogin
+  const isLogin = store.getters.isLogin
   if (isLogin) {
     const token = store.state.token
     request.headers.Authorization = `Bearer ${token}`
