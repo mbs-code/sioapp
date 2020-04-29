41db0e82bf5cb85edd5965de282d9bb303918ade
mabasasi <aoe317293@yahoo.co.jp>
2020-04-29 12:41:59 +0900

    [add] localstorage の暗号化
    ・add secure-ls

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
+import SecureLS from "secure-ls"
 
 import auth from './auth'
 import config from './config'
 
 Vue.use(Vuex)
 
+
+const ls = new SecureLS({ encordingType: 'AES', isCompression: false })
+
 export default new Vuex.Store({
   modules: { auth, config },
 
-  plugins: [createPersistedState()] // localstorage 永続化
+  // localstorage 永続化
+  plugins: [createPersistedState({
+    storage: {
+      getItem: key => ls.get(key),
+      setItem: (key, value) => ls.set(key, value),
+      removeItem: key => ls.remove(key)
+    }
+  })]
 })
