92c069e63e231f3226868315d8bff735f3eadf14
mabasasi <aoe317293@yahoo.co.jp>
2020-05-10 06:38:32 +0900

    [fix] コメントアウト

--- src/components/parts/PEventSwitch.vue
+++ src/components/parts/PEventSwitch.vue
@@ -57,7 +57,6 @@ export default {
       if (val) {
         // true なら 新規タイマーを作成（念のため削除しておく）
         const seconds = Number(this.seconds) * 1000
-        console.log(`auto update: ${seconds} sec.`)
 
         clearInterval(this.timerObj)
         this.timerObj = setInterval(() => { this.onFinish() }, seconds)
@@ -65,8 +64,6 @@ export default {
       } else {
         // false なら タイマーを削除
         clearInterval(this.timerObj)
-        console.log('auto update: clear')
-
         this.timerObj = null
         this.timerOn = false
       }
@@ -74,7 +71,6 @@ export default {
 
     // タイマー終了時の処理
     onFinish () {
-      console.log('> on Finish!')
       this.$emit('timer')
     }
   }
